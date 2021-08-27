8d297745ba78d2b147438adf124d9abaa7fce7f5
Ken Gregory <kgreg80@gmail.com>
2021-08-27 15:36:14 -0400

    Fix most TypeScript errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Demo from "./demo";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Demo from './demo';
-
-ReactDOM.render(<Demo />, document.querySelector('#root'));
-    
+ReactDOM.render(<Demo />, document.querySelector("#root"));
